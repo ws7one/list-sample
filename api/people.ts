1ec1056a7858e635f5a27984d99e16e1a87aa674
Philip Viningston <philip.viningston@reasonlabs.com>
2025-01-27 01:10:52 +0400

    Implementing a local mocker for the API

--- api/people.ts
+++ api/people.ts
@@ -1,12 +1,15 @@
-import axios from "axios";
-import { ATTENDEES } from "./urls";
+// import axios from "axios";
+// import { ATTENDEES } from "./urls";
+import { mockPeople } from "@/types/people";
 
 export const getPeople = async () => {
-  const key = "a7513cc0";
+  // const key = "a7513cc0";
 
   try {
-    const response = await axios.get(`${ATTENDEES}?key=${key}`);
-    return response.data;
+    // const response = await axios.get(`${ATTENDEES}?key=${key}`);
+    // return response.data;
+    const response = mockPeople();
+    return response;
     // return list
   } catch (error) {
     console.error(error);
--- types/people.ts
+++ types/people.ts
@@ -1,3 +1,5 @@
+import { faker } from "@faker-js/faker";
+
 export interface People {
   id: string;
   name: string;
@@ -8,3 +10,16 @@ export interface People {
   number_of_guests: number;
   address: string;
 }
+
+export const mockPeople = (count: number = 10): People[] => {
+  return Array.from({ length: count }, () => ({
+    id: faker.string.uuid(),
+    name: `${faker.person.firstName()} ${faker.person.lastName()}`,
+    age: Math.floor(faker.number.int({ min: 18, max: 80 })),
+    date_of_birth: faker.date.past().toISOString().split("T")[0],
+    employed: faker.datatype.boolean(),
+    locality: faker.address.city(),
+    number_of_guests: faker.number.int({ min: 1, max: 10 }),
+    address: `${faker.address.streetAddress()} ${faker.address.city()}, ${faker.address.state()} ${faker.address.zipCode()}`,
+  }));
+};
